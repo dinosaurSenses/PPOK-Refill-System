4d33bc11208af336dec7e93318617274509ac64e
tuyishime <oliviertyshime@gmail.com>
2017-02-27 14:33:00 -0600

    Initial setup

---
+++ ppok_refill_system.UI/ppok_refill_system.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ppok_refill_system.UI.MvcApplication" Language="C#" %>
---
+++ ppok_refill_system.UI/ppok_refill_system.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ppok_refill_system.UI.Startup))]
+namespace ppok_refill_system.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
